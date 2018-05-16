0f6e50262caac4e73cea28faacc4a641d6db2dc9
Hedi van Meurs <alisoun@gmail.com>
2018-05-16 16:48:18 +0100

    works, not styled

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '0712345678',
+    category:     'Indian'
+
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '0712345678',
+    category:     'Italian'
+  },
+{
+    name:         "La Tour d'Argent",
+    address:       "15 Quai de la Tournelle, 75005 Paris",
+    phone_number:  "01 43 54 23 31",
+    category:      'french'
+  },
+  {
+    name: 'Dou Dou',
+    address: 'Camden',
+    phone_number: '0712345678',
+    category: 'chinese'
+  },
+  {
+    name: '222 Vegan',
+    address: 'West-Brompton',
+    phone_number: '0712345678',
+    category: 'fusion'
+  }
+
+
+]
+# Restaurant.create!(restaurants_attributes)
+restaurants_attributes.each do |attribute|
+  Restaurant.create(attribute)
+end
+puts 'Finished!'
